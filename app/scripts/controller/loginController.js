17ef35ac011ee54ca72c08a8e8c8f36a8ca112d2
Rorro <juan.cistaro@gmail.com>
2015-06-03 08:25:09 -0300

    Request a la API de Unify

--- app/scripts/controller/loginController.js
+++ app/scripts/controller/loginController.js
@@ -2,7 +2,13 @@
 
 var unifyApp = angular.module('unifyApp.controllers');
 
-unifyApp.controller("loginController", function ($scope, $location, $translate, ENV) {
+unifyApp.controller("loginController", function ($scope, $http, $location, $translate, ENV) {
 
-
+    $scope.login = function () {
+        console.log("PROBANDO!");
+        $http.get('http://localhost:8080/auth/facebook').success(function (data) {
+                //$location.path("/bookings");
+                return data;
+            });
+    };
 });
