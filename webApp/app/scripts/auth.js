226415b8cbc1a298604618ec90c1475b1ab79674
Rorro <juan.cistaro@gmail.com>
2015-06-27 19:28:46 -0300

    baseurl api auth

--- webApp/app/scripts/auth.js
+++ webApp/app/scripts/auth.js
@@ -1,24 +1,25 @@
 'use strict';
 
 angular.module('unifyApp').config(function($authProvider, ENV) {
+    $authProvider.baseUrl = ENV.apiEndPoint;
 
     $authProvider.facebook({
       clientId: '805638479520745',
-      url: ENV.apiEndPoint + '/auth/facebook'
+      url: '/auth/facebook'
     });
 
     $authProvider.google({
       clientId: '79996335280-gc0hh1efoo859u1lqaqct2v3u1larsrj.apps.googleusercontent.com',
-      url: ENV.apiEndPoint + '/auth/google'
+      url: '/auth/google'
     });
 
    $authProvider.twitter({
-      url: ENV.apiEndPoint + '/auth/twitter'
+      url: '/auth/twitter'
     });
 
     $authProvider.oauth2({
       name: 'instagram',
-      url: ENV.apiEndPoint + '/auth/instagram',
+      url: '/auth/instagram',
       redirectUri: window.location.origin,
       clientId: 'ad148c3db70f4b7188a0e30c1b74ea06',
       requiredUrlParams: ['scope'],
