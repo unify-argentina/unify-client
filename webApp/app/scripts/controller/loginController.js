626783cdc65ff37df9d1782458bf62e032afe774
Rorro <juan.cistaro@gmail.com>
2015-06-27 18:38:58 -0300

    login prototype

--- webApp/app/scripts/controller/loginController.js
+++ webApp/app/scripts/controller/loginController.js
@@ -4,10 +4,17 @@ var unifyApp = angular.module('unifyApp.controllers');
 
 unifyApp.controller("loginController", function ($scope, $http, $auth, $state, $translate, ENV) {
 
-  $scope.prueba = function () {
-    $http.get('http://localhost:9000/pedo')
-      .success(function (data) {
-          console.log(data);
+  $scope.login = function() {
+    $auth.login({ 
+      email: $scope.user.email, 
+      password: $scope.user.password 
+    }).then(function() {
+        localStorage.setItem('response', JSON.stringify(response));
+        console.log('You have successfully logged in: '+response.data); 
+        $state.go('dashboard');
+      })
+      .catch(function(response) {
+        console.log(response.data ? response.data.message : response);
       });
   };
 
@@ -21,34 +28,5 @@ unifyApp.controller("loginController", function ($scope, $http, $auth, $state, $
       .catch(function(response) {
         console.log(response.data ? response.data.message : response);
       });
-    };
-
-        /**
-     * Link third-party provider.
-     */
-    $scope.link = function(provider) {
-      $auth.link(provider)
-        .then(function() {
-           console.log('You have successfully linked ' + provider + ' account');
-        })
-        .catch(function(response) {
-          console.log(response.data.message);
-        });
-    };
-
-    /**
-     * Unlink third-party provider.
-     */
-    $scope.unlink = function(provider) {
-      $auth.unlink(provider)
-        .then(function() {
-          console.log('You have successfully unlinked ' + provider + ' account');
-        })
-        .then(function() {
-          $scope.getProfile();
-        })
-        .catch(function(response) {
-          console.log(response.data ? response.data.message : 'Could not unlink ' + provider + ' account');
-        });
-    };
+  };
 });
