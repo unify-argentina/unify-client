232e3f47bd482c0dedaebc8a06c84622bd35c8e9
Joel Márquez <joelm@hellobaytree.com>
2015-09-25 16:55:12 -0300

    Mac fix

--- desktopClient/app/scripts/app.js
+++ desktopClient/app/scripts/app.js
@@ -3,6 +3,5 @@ require('nw.gui').Window.get().maximize();
 $(document).ready(function() {
 	window.location.href="http://test-unify.herokuapp.com/"
 	window.setTimeout(function(){
-		
 	},100);
 });
